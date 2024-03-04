be324c477aa7bb50b775dd2870d2a5ac613b1a43
sdkoq <159871493+sdkoq@users.noreply.github.com>
2024-03-04 11:44:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ import os
 
 from os import environ
 
-API_ID = int(os.environ.get('API_ID', '25957229'))
-API_HASH = os.environ.get("API_HASH", "f19e0303f0d218f1744a28b826b7dc96")
+API_ID = int(os.environ.get('API_ID', '25339665'))
+API_HASH = os.environ.get("API_HASH", "979cab179b80d3a826a27ab0c16dcc4a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6711385952:AAHh6_HrAFPU6qZeYXfXv__CDt-v2EdJq2U")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6320511438:AAHhA0AVfWPLAuKVnc7HGHzxpEmYZILh6rU")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6446539222))
+OWNER = int(os.environ.get("OWNER", 5997896353))
 
-LOG = int(environ.get('LOG', '-1002124099587'))
+LOG = int(environ.get('LOG', '-1001699267852'))
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6446539222").split()):
+    for x in (os.environ.get("ADMINS", "5997896353").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
